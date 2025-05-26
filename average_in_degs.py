1e34db2eed7eb263b468ca03c628860d39b85258
infeter-cmd <73298664+infeter-cmd@users.noreply.github.com>
2025-05-26 22:45:55 +0300

    add new func

---
+++ average_in_degs.py
@@ -0,0 +1,13 @@
+# ------
+def average_in_degs(arr_in_degs):
+    N = len(arr_in_degs)
+    keys = set()
+    result = {}
+    for in_degs in arr_in_degs:
+        keys |= in_degs.keys()
+    for key in keys:
+        result[key] = 0
+        for in_degs in arr_in_degs:
+            result[key] += in_degs.get(key, default=0)
+        result[key] /= N
+    return result
