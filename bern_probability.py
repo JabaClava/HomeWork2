8e4b72304346e52ea3a4674f231b2d097caf06cf
infeter-cmd <73298664+infeter-cmd@users.noreply.github.com>
2025-05-28 14:47:22 +0300

    remove bug

--- bern_probability.py
+++ bern_probability.py
@@ -1,15 +1,18 @@
 def bern_probability(x, n, p):
     result = 1
+    q = 1 - p
     if x > n - x:
         for i in range(n - x + 1):
-            result *= p * (1 - p) / i
+            result *= p * q / i
         for i in range(n - x + 1, x + 1):
-            result *= p * (1 - p)
+            result *= p
         for i in range(x + 1, n + 1):
             result *= i
         return result
     for i in range(x + 1):
-        result *= p * (1 - p) / i
+        result *= p * q / i
+    for i in range(x + 1, n - x + 1):
+        result *= q
     for i in range(n - x + 1, n + 1):
         result *= i
     return result
